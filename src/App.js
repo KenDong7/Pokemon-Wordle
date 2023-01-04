16dcc13be15e0140d6c5d3bc15601089c3416223
Ken Dong <117216342+KenDong7@users.noreply.github.com>
2023-01-04 18:24:04 -0500

    rules

--- src/App.js
+++ src/App.js
@@ -18,6 +18,7 @@ function App() {
 
   useEffect(() => {
     getTargetPokemon();
+    document.title = "Who's That Pokémon?";
   }, []);
   
   const getTargetPokemon = async () => {
@@ -70,60 +71,62 @@ function App() {
     getPokemon()
   }
 
-  /*useEffect(() => {
-    isCorrect()
-  }, [pokemonData]);
-  
-  const isCorrect = ()  => {
-    if (pokemonData[0] != null && targetData[0] != null) {
-      if (pokemonData[0].name == targetData[0].name) {
-        alert(`Guessed in ${counter} Tries`)
-      }
-    }
-  }*/
- 
-  return (
-    <div className="App">
-      <h1>Who's That Pokémon?</h1>
-      <form onSubmit = {handleSubmit}>
-        <label>
-          <input type = "text" onChange = {handleChange} placeholder = "Enter Pokemon Name">
-          </input>
-        </label>
-      </form>
-      {pokemonData.map((data) =>  {
-        return(
-          <div className = "container">
-            <img src = {data.sprites["front_default"]}/>
-            <div className = "table"> 
-              <div className = "tableBody">
 
-              <div className = "tableRow"> 
-                <div className = "tableCell"> Type 1 </div>
-                <CorrectType1 targetType = {targetType[0]} chosenType = {pokemonType[0]}/>
+  return (
+    
+    <div className="App">
+      <div className = "header"> 
+        <div className = "rules"> 
+          <p id = "rules"> RULES:</p>
+          <ul>
+            <p className = "correct">Correct</p>
+            <p className = "wrong">Incorrect</p>
+            <p className = "high">Too High</p>
+            <p className = "low">Too Low</p>
+          </ul>
+          
+        </div>
+        <h1>Who's That Pokémon?</h1>
+      </div>
+        <form onSubmit = {handleSubmit}>
+          <label>
+            <input type = "text" onChange = {handleChange} placeholder = "Enter Pokemon Name">
+            </input>
+          </label>
+        </form>
+        {pokemonData.map((data) =>  {
+          return(
+            <div className = "container">
+              <img src = {data.sprites["front_default"]}/>
+              <div className = "table"> 
+                <div className = "tableBody">
+
+                <div className = "tableRow"> 
+                  <div className = "tableCell"> Type 1 </div>
+                  <CorrectType1 targetType = {targetType[0]} chosenType = {pokemonType[0]}/>
+                </div>
+                <div className = "tableRow"> 
+                  <div className = "tableCell"> Type 2 </div>
+                  <CorrectType2 targetType = {targetType[1]} chosenType = {pokemonType[1]}/>
+                </div>
+                <div className = "tableRow"> 
+                  <div className = "tableCell"> Height </div>
+                  <CorrectHeight targetHeight = {Math.round(targetData[0].height * 3.93701)} chosenHeight = {Math.round(data.height * 3.93701)}/>
+                </div>
+                <div className = "tableRow"> 
+                  <div className = "tableCell"> Weight </div>
+                  <CorrectWeight targetWeight = {Math.round(targetData[0].weight / 4.53592 * 10) / 10} chosenWeight = {Math.round(data.weight / 4.53592 * 10) / 10}/>
+                </div>
+                <Result targetName = {targetData[0].name} chosenName = {pokemonData[0].name} counter = {counter}/>
               </div>
-              <div className = "tableRow"> 
-                <div className = "tableCell"> Type 2 </div>
-                <CorrectType2 targetType = {targetType[1]} chosenType = {pokemonType[1]}/>
-              </div>
-              <div className = "tableRow"> 
-                <div className = "tableCell"> Height </div>
-                <CorrectHeight targetHeight = {Math.round(targetData[0].height * 3.93701)} chosenHeight = {Math.round(data.height * 3.93701)}/>
-              </div>
-              <div className = "tableRow"> 
-                <div className = "tableCell"> Weight </div>
-                <CorrectWeight targetWeight = {Math.round(targetData[0].weight / 4.53592 * 10) / 10} chosenWeight = {Math.round(data.weight / 4.53592 * 10) / 10}/>
-              </div>
-              <Result targetName = {targetData[0].name} chosenName = {pokemonData[0].name} counter = {counter}/>
             </div>
           </div>
+          )
+        })}
+        <div className = "container">
+          <a className href="https://pokemondb.net/pokedex/stats/height-weight">Pokémon Database</a>
         </div>
-        )
-      })}
-      <div className = "container">
-        <a className href="https://pokemondb.net/pokedex/stats/height-weight">Pokémon Database</a>
-      </div>
-     
+      
     </div>
   );
 }
